6ac28307721de471cd5ae282e4ae64d8286fdd3b
Murthy Garimella <mgarimella@g.harvard.edu>
2021-10-27 21:38:18 +0000

    week9

---
+++ rps/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7c7d778c63dbfa25a7fbdf19c90b760f9ceae512',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7c7d778c63dbfa25a7fbdf19c90b760f9ceae512',
+            'dev_requirement' => false,
+        ),
+        'susanbuck/rock-paper-scissors' => array(
+            'pretty_version' => '0.1.5',
+            'version' => '0.1.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../susanbuck/rock-paper-scissors',
+            'aliases' => array(),
+            'reference' => '2b8a34339715507b1b9f7664478ff51bdc9ff219',
+            'dev_requirement' => false,
+        ),
+    ),
+);
