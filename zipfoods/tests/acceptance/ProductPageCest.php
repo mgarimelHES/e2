9eb22c3ac20b1296d3e27195fc206130cfcdab61
Murthy Garimella <mgarimella@g.harvard.edu>
2021-12-03 17:50:32 +0000

    week14 - lessons practice

---
+++ zipfoods/tests/acceptance/ProductPageCest.php
@@ -0,0 +1,25 @@
+<?php
+
+class ProductPageCest
+{
+    /**
+     * Test that we can load the product page and see the expected content
+     */
+    public function pageLoads(AcceptanceTester $I)
+    {
+        # Act
+        $I->amOnPage('/product?sku=driscolls-strawberries');
+
+        # Assert the correct title is set on the page
+        $I->seeInTitle('Driscoll’s Strawberries');
+
+        # Assert the existence of certain text on the page
+        $I->see('Driscoll’s Strawberries');
+
+        # Assert the existence of a certain element on the page
+        $I->seeElement('.product-thumb');
+
+        # Assert the existence of text within a specific element on the page
+        $I->see('$4.99', '.product-price');
+    }
+}
